d1b82c3e256445c204ac5fad3b83d942bbab9d31
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-10-03 14:49:34 +0530

    Initial commit

---
+++ CapX/CapXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapXApp.swift
+//  CapX
+//
+//  Created by Abhay singh on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CapXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CapX
+//
+//  Created by Abhay singh on 03/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
